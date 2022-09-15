c4b69fefc125beb514de2be9279f108ef5620ecd
milad.xsar23 <milad.xsar72@gmail.com>
2022-09-15 17:22:14 +0430

    add mutation to schema

--- server.js
+++ server.js
@@ -1,27 +1,72 @@
 // dependencies
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const { GraphQLObjectType, GraphQLSchema, GraphQLString } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLID,
+  GraphQLNonNull,
+} = require("graphql");
 // server configurations
 const server = express();
 const port = 3000;
+// ----- Root Query
 const RootQuery = new GraphQLObjectType({
-    name : "RootQuery", 
-    description:"the root query", 
-    fields : {
-        viewer : {
-            type : GraphQLString, 
-            resolve() {
-                return "viewer!"
-            }
+  name: "RootQuery",
+  description: "the root Query",
+  fields: {
+    viewer: {
+      type: GraphQLString,
+      resolve() {
+        return "viewer";
+      },
+    },
+    node: {
+      type : GraphQLString, 
+      args : {
+        id : {
+          type : new GraphQLNonNull(GraphQLID)
         }
+      }, 
+      resolve(source, args) {
+        return inMemoryStore[args.id]
+      }
     }
-})
-const Schema = new GraphQLSchema({
-    query : RootQuery
+  },
+});
+// ------- Mutations
+let inMemoryStore = Object.create(null)
+const RoouMutation = new GraphQLObjectType({
+  name : "RootMutation", 
+  description : "the root mutation", 
+  fields : {
+    setNode : {
+      type : GraphQLString, 
+      args : {
+        id : {
+          type : new GraphQLNonNull(GraphQLID)
+        }, 
+        value : {
+          type : new GraphQLNonNull(GraphQLString)
+        }
+      }, 
+      resolve(source, args) {
+        inMemoryStore[args.id] = args.value
+        console.log(inMemoryStore)
+        return inMemoryStore[args.id]
+      }
+    }
+  }
 })
 
-server.use("/graphql", graphqlHTTP({schema : Schema, graphiql : true}));
+//  ------- Schema
+const Schema = new GraphQLSchema({
+  query: RootQuery,
+  mutation : RoouMutation
+});
+
+server.use("/graphql", graphqlHTTP({ schema: Schema, graphiql: true }));
 
 server.listen(port, () => {
   console.log("server run in http://localhost:3000");
